e7ab34c5ff79ee454b84ccb85fb7d75e6f955e97
EduardoLobo23 <eduardo.lobo@tecnico.ulisboa.pt>
2023-05-15 21:02:35 +0100

    add base html and js files

---
+++ js/robot.js
@@ -0,0 +1,78 @@
+var camera, scene, renderer;
+
+var mesh;
+
+//var ball;
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+
+
+    scene.add(new THREE.AxisHelper(10));
+
+    //createTable(0, 8, 0);
+    //createBall(0, 0, 15);
+}
+
+function createCamera() {
+    'use strict';
+    camera = new THREE.PerspectiveCamera(70,
+                                         window.innerWidth / window.innerHeight,
+                                         1,
+                                         1000);
+    camera.position.x = 50;
+    camera.position.y = 50;
+    camera.position.z = 50;
+    camera.lookAt(scene.position);
+}
+
+function onResize() {
+    'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
+
+}
+
+function render() {
+    'use strict';
+    renderer.render(scene, camera);
+}
+
+function init() {
+    'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+}
+
+function animate() {
+    'use strict';
+
+    /*
+    if (ball.userData.jumping) {
+        ball.userData.step += 0.04;
+        ball.position.y = Math.abs(30 * (Math.sin(ball.userData.step)));
+        ball.position.z = 15 * (Math.cos(ball.userData.step));
+    }
+    */
+    render();
+
+    requestAnimationFrame(animate);
+}v
